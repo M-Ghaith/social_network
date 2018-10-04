9836db1a823509ae128968d4c601d96294a203bc
MG Alseirawan <ghaithprincegh@gmail.com>
2018-10-04 21:43:13 +0200

    Added user routes and user schema

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const port = process.env.PORT || 1337;
 const mongoos = require("mongoose");
+const bodyParser = require("body-parser");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
+
+//URL middleware handler
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB config
 const db = require("./config/keys").mongoURL;
@@ -13,6 +22,11 @@ mongoos
   .then(() => console.log("Database connected."))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hi Ghaith, sup?"));
+app.get("/", (req, res) => res.send("Hi Ghaith, Sup?"));
+
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
